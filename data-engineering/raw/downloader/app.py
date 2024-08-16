60d946a860e5670b4dec63153e5e7f17995f785a
Cristhian <crijpardomer@unal.edu.co>
2024-08-15 19:36:56 -0500

    unzip lambda initial logic created

--- data-engineering/raw/downloader/app.py
+++ data-engineering/raw/downloader/app.py
@@ -1,31 +1,34 @@
 import os
-import time
 import requests
 import boto3
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-# from botocore.exceptions import BotoCoreError, ClientError
-
-# Retry setup for requests
-retry_strategy = Retry(
-    total=5,  # Total retries
-    backoff_factor=1,  # Exponential backoff factor (1, 2, 4, 8, etc.)
-    status_forcelist=[429, 500, 502, 503, 504],  # HTTP codes to retry
-    allowed_methods=["HEAD", "GET", "OPTIONS"]  # Methods to retry
-)
-adapter = HTTPAdapter(max_retries=retry_strategy)
-http = requests.Session()
-http.mount("http://", adapter)
-http.mount("https://", adapter)
 
 s3_client = boto3.client('s3')
+RAW_DATA_BUCKET_NAME = os.getenv("RAW_DATA_BUCKET_NAME")
 
 def lambda_handler(event, context):
     file_name = event['file_name']
     type = event['type']
-
     url = f"http://data.gdeltproject.org/{type}/{file_name}"
+    key = f"{type}/{file_name}"
+    s3_uri = f"s3://{RAW_DATA_BUCKET_NAME}/{key}"
+    try:
+        response = requests.get(url)
+        try:
+            s3_client.put_object(
+                Bucket=RAW_DATA_BUCKET_NAME,
+                Key=key,
+                Body=response.content
+            )
+            print("Successfully uploaded!")
+            
+        except Exception as e:
+            print(f"Failed, error: {e}")
+
+    except Exception as e:
+        print(f"Failed to download {file_name} from {url}, error: {e}")
+    
     return {
         "statusCode": 200,
-        "body": url
+        "url": url,
+        "s3_uri": s3_uri
     }
--- data-engineering/raw/zip_processor/app.py
+++ data-engineering/raw/zip_processor/app.py
@@ -1,5 +1,28 @@
+import boto3
+import os
+import zipfile
+import io
+
+s3_client = boto3.client('s3')
+UNZIPPED_BUCKET_NAME = os.environ['UNZIPPED_BUCKET_NAME']
+
 def lambda_handler(event, context):
+    raw_bucket = event['Records'][0]['s3']['bucket']['name']
+    zip_key = event['Records'][0]['s3']['object']['key']
+    
+    zip_obj = s3_client.get_object(Bucket=raw_bucket, Key=zip_key)
+    buffer = io.BytesIO(zip_obj["Body"].read())
+    
+    with zipfile.ZipFile(buffer, 'r') as zip_ref:
+        for file_name in zip_ref.namelist():
+            extracted_file = zip_ref.open(file_name)
+            s3_client.upload_fileobj(
+                extracted_file,
+                UNZIPPED_BUCKET_NAME,
+                file_name
+            )
+    
     return {
-        "statusCode": 200,
-        "body": "Hello from Lambda!"
+        'statusCode': 200,
+        'body': f'Successfully extracted and uploaded files'
     }
