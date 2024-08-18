a560992a426b130e8d5db0acfa3ee2fe0b0b5cfe
dmeloca <melocarrillodaniel@gmail.com>
2024-08-18 18:34:38 -0500

    feat(main): extract paragraphs

--- web-scrapping/scraper/src/main.js
+++ web-scrapping/scraper/src/main.js
@@ -1,16 +1,42 @@
 import { CheerioCrawler } from 'crawlee';
 import { URL } from 'node:url';
 import fs from 'fs';
+import path from 'path';
+//const data = fs.readFileSync('source_urls.json', 'utf8');
+//const urls_list = JSON.parse(data);
 
-const data = fs.readFileSync('source_urls.json', 'utf8');
-const urls_list = JSON.parse(data);
-
+function sanitizeTitle(title) {
+    return title.replace(/[\/\\?%*:|"<>]/g, '_').trim() + '.json';
+}
 
 const crawler = new CheerioCrawler({
     maxRequestsPerCrawl: 20,
     async requestHandler({ request, $ }) {
+        //const title = $('title').text();
+        //console.log(`The title of "${request.url}" is: ${title}.`);
+        // Obtén el título de la página
         const title = $('title').text();
-        console.log(`The title of "${request.url}" is: ${title}.`);
+        // Extrae el contenido de todos los elementos <p>
+        const paragraphs = $('p').map((i, el) => $(el).text()).get();
+        
+        // Crea un objeto con el título y los párrafos
+        const data = {
+            title: title,
+            content: paragraphs
+        };
+        const fileName = sanitizeTitle(title);
+        const directory = 'extraction';
+        const filePath = path.join(directory, fileName);
+        
+        // Asegúrate de que la carpeta exista
+        if (!fs.existsSync(directory)) {
+            fs.mkdirSync(directory, { recursive: true });
+        }
+
+        // Guarda el objeto en un archivo JSON
+        fs.writeFileSync(filepath, JSON.stringify(data, null, 2), 'utf-8');
+        
+        console.log('Datos guardados en output.json');
 
         const links = $('a[href]')
             .map((_, el) => $(el).attr('href'))
@@ -32,4 +58,5 @@ const crawler = new CheerioCrawler({
     },
 });
 
-await crawler.run([...urls_list])
+// await crawler.run([...urls_list])
+await crawler.run(["https://www.eldoradonews.com/news/2023/aug/13/sanders-picks-new-secretary-of-state-parks/"])
