0b33edb169087e123020e29f4dc1dce01354b9f8
Cristhian <crijpardomer@unal.edu.co>
2024-08-24 16:30:44 -0500

    The urls_availability service logic is changed so that the get_url method now receives a queryStringParameters called batch_size. The post_url method can delete more than one message

--- data-engineering/urls_availability/get_url/app.py
+++ data-engineering/urls_availability/get_url/app.py
@@ -5,24 +5,33 @@ import json
 sqs = boto3.client('sqs')
 queue_url = os.environ['QUEUE_URL']
 
+
 def lambda_handler(event, context):
+    batch_size = event.get('queryStringParameters', {}).get('batch_size', 1)
+    batch_size = int(batch_size)
+    batch_size = min(batch_size, 10)
+    
     response = sqs.receive_message(
         QueueUrl=queue_url,
-        MaxNumberOfMessages=1,
+        MaxNumberOfMessages=batch_size,
         WaitTimeSeconds=10
     )
     
     if 'Messages' in response:
-        message = response['Messages'][0]
-        receipt_handle = message['ReceiptHandle']
-        body = json.loads(message['Body'])
-        body["receipt_handle"] = receipt_handle
+        messages = []
+        for message in response['Messages']:
+            receipt_handle = message['ReceiptHandle']
+            body = json.loads(message['Body'])
+            body["receipt_handle"] = receipt_handle
+            messages.append(body)
+        
         return {
             'statusCode': 200,
-            'body': json.dumps(body)
-        }
+            'body': json.dumps(messages)
+        }    
     else:
+        body = json.dumps({'message': 'No messages available in the queue'})
         return {
             'statusCode': 404,
-            'body': json.dumps({'message': 'No messages available in the queue'})
+            'body': body
         }
--- data-engineering/urls_availability/post_url/app.py
+++ data-engineering/urls_availability/post_url/app.py
@@ -7,14 +7,17 @@ queue_url = os.environ['QUEUE_URL']
 
 def lambda_handler(event, context):
     body = json.loads(event['body'])
-    receipt_handle = body['receipt_handle']
-    
-    sqs.delete_message(
-        QueueUrl=queue_url,
-        ReceiptHandle=receipt_handle
-    )
+
+    # We expect the body to be a list of dictionaries, each with a
+    # receipt_handle
+    for message in body:
+        receipt_handle = message['receipt_handle']
+        sqs.delete_message(
+            QueueUrl=queue_url,
+            ReceiptHandle=receipt_handle
+        )
     
     return {
         'statusCode': 200,
-        'body': json.dumps({'message': 'Message deleted successfully'})
+        'body': json.dumps({'message': 'Messages deleted successfully'})
     }
