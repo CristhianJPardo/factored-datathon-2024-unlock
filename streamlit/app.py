b1045bb5124c0ed252d5a7e90b1f15622e2fffef
dmeloca <melocarrillodaniel@gmail.com>
2024-08-25 18:41:52 -0500

    feature(app): add status pages

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 st.set_page_config(
     page_title="Unlock",
     page_icon="static/dollar.png",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="auto",
 )
 # If you want to add a logo to the page, uncomment the line below and add the path to the image
@@ -21,10 +21,11 @@ st.write(f"Last updated: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 st.divider()
 
 ## Navigation ##
+status = st.Page("tools/status.py", title="Status", icon=":material/insert_chart_outlined:")
 search = st.Page("tools/search.py", title="Search", icon=":material/search:")
 chat = st.Page("tools/chat.py", title="Chat", icon=":material/chat:")
 visualization = st.Page("tools/visualization.py", title="Visualization", icon=":material/dashboard:")
-pg = st.navigation({"Tools": [search, chat, visualization]})
+pg = st.navigation({"Tools": [status, search, chat, visualization]})
 
 ## Run Page ##
 pg.run()
