f30431c505c372cfd751728c188d846a555ed5ed
Cristhian <crijpardomer@unal.edu.co>
2024-08-25 18:57:04 -0500

    deltasharing added to streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,5 +1,53 @@
 import streamlit as st
 from datetime import datetime
+import os
+import json
+from dotenv import load_dotenv
+import delta_sharing
+
+load_dotenv()  # take environment variables from .env.
+
+#########################################################
+SHARECREDENTIALSVERSION = os.getenv("SHARECREDENTIALSVERSION")
+BEARERTOKEN = os.getenv("BEARERTOKEN")
+ENDPOINT = os.getenv("ENDPOINT")
+EXPIRATIONTIME = os.getenv("EXPIRATIONTIME")
+
+credentials = {
+	"shareCredentialsVersion": SHARECREDENTIALSVERSION,
+	"bearerToken": BEARERTOKEN,
+	"endpoint": ENDPOINT,
+	"expirationTime": EXPIRATIONTIME,
+}
+
+file_name = "config.share"
+
+
+# Write the dictionary to a JSON file in a single line
+with open(file_name, 'w') as json_file:
+    json.dump(credentials, json_file, separators=(',', ':'), ensure_ascii=False)
+
+
+# Point to the profile file. It can be a file on the local file system or a file on a remote storage.
+profile_file = file_name
+
+# Create a SharingClient.
+client = delta_sharing.SharingClient(profile_file)
+
+# List all shared tables.
+print(client.list_all_tables())
+
+news_per_day = profile_file + "#unlock-share-streamlit.platinum.news_per_day"
+total_database_size = profile_file + "#unlock-share-streamlit.platinum.total_database_size"
+
+df_news_per_day = delta_sharing.load_as_pandas(news_per_day)
+df_total_database_size = delta_sharing.load_as_pandas(total_database_size)
+
+
+print(df_news_per_day.head())
+# TODO: GUARDAR DATOS EN CACHE
+############################################################
+
 #################
 ## Page Config ##
 #################
